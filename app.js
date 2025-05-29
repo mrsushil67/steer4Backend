39fd64a48f49e8ae5415ea6da65d4c99d0959d9d
mrsushil67 <mrsushil67@gmail.com>
2025-05-29 15:06:05 +0530

    add body parser

--- app.js
+++ app.js
@@ -1,12 +1,14 @@
 const express = require("express");
 const app = express();
 const MainRouter = require('./routes/mainRoute');
+const bodyParser = require('body-parser');
 const morgan = require("morgan");
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 app.use(express.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(morgan('dev'));
 app.use("/api/v1",MainRouter);
 
