e0fceaca6fd0e5c7dd6b6cf6482a96b9b1339ee4
mrsushil67 <mrsushil67@gmail.com>
2025-05-29 12:46:47 +0530

    add morgan to api log

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
 const express = require("express");
 const app = express();
-const MainRouter = require('./routes/mainRoute')
+const MainRouter = require('./routes/mainRoute');
+const morgan = require("morgan");
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 app.use(express.json());
+app.use(morgan('dev'));
 app.use("/api/v1",MainRouter);
 
 module.exports = app;
