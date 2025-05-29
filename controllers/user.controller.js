45de289910db6546375e2183245b810c2f5fa729
mrsushil67 <mrsushil67@gmail.com>
2025-05-29 11:38:39 +0530

    update function

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -5,11 +5,18 @@ const jwt = require('jsonwebtoken');
 function generateMD5Hash(data) {
   const hash = crypto.createHash('md5');
   hash.update(data);
-  return hash.digest('hex'); // Returns a hexadecimal string
+  return hash.digest('hex');
 }
 
-function generateToken(user){
-    const token = jwt.sign({userId: user.User_ID, Email: user.Email}, process.env.JWTSECRET,{expiresIn: '20m'});
+function generateToken(user) {
+    if (!process.env.JWTSECRET) {
+        throw new Error("JWTSECRET is not defined in environment variables");
+    }
+    const token = jwt.sign(
+        { userId: user.User_ID, Email: user.Email },
+        process.env.JWTSECRET,
+        { expiresIn: '20m' }
+    );
     return token;
 }
 
@@ -18,7 +25,7 @@ module.exports.signInUser = async (req, res) => {
         const { Email, password } = req.body;
 
         if (!Email || !password) {
-            return res.status(400).json({ message: "Fill all fields", status: false });
+            return res.status(400).json({  status: "400", message: "Fill all fields"});
         }
 
         const user = await DBMODELS.Users.findOne({
@@ -26,17 +33,17 @@ module.exports.signInUser = async (req, res) => {
         });
 
         if (!user) {
-            return res.status(404).json({ message: "Invalid User", status: false });
+            return res.status(404).json({ status: "404", message: "Invalid User"});
         }
 
         const hashpassword = generateMD5Hash(password);
 
         if (hashpassword !== user.password) {
-            return res.status(401).json({ message: "Invalid Password", status: false });
+            return res.status(401).json({  status: "401", message: "Invalid Password"});
         }
 
         const token = generateToken(user);
-        return res.status(200).json({ message: "User logged in successfully", user, token });
+        return res.status(200).json({ status: "200", message: "User logged in successfully", user, token });
     } catch (error) {
         console.error(error);
         return res.status(500).json({ message: "Internal Server Error", status: false });
