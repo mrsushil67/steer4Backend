6042208ce0c5164a680420425d2205c850178f2c
mrsushil67 <mrsushil67@gmail.com>
2025-06-02 13:26:07 +0530

    update function

--- controllers/route.controller.js
+++ controllers/route.controller.js
@@ -2,27 +2,26 @@ const { Op } = require("sequelize");
 const { DBMODELS } = require("../models/init-models");
 
 module.exports.getRoutelist = async (req, res) => {
-  try {
-    const { CustId = null, RouteCat = null } = req.body || {};
+    try {
+        const { CustId = null, RouteCat = null } = req.body || {};
 
-    const whereCondition =
-      CustId || RouteCat
-        ? {
-            [Op.or]: [
-              { CustId: CustId },
-              { RouteCat: RouteCat }
-            ]
-          }
-        : {};
+        const whereCondition = {};
+        if (CustId) whereCondition.CustId = CustId;
+        if (RouteCat) whereCondition.RouteCat = RouteCat;
 
-    const routes = await DBMODELS.RouteMaster.findAll({
-      where: whereCondition,
-    });
-    return res
-      .status(200)
-      .json({ status: "200", message: "Record found", routes });
-  } catch (error) {
-    console.log(error);
-    return res.status(500).json("Internal server error");
-  }
+        const routes = await DBMODELS.RouteMaster.findAll({
+            where: whereCondition,
+        });
+
+        if (routes.length === 0) {
+            return res.status(404).json({ status: "404", message: "No records found" });
+        }
+
+        return res
+            .status(200)
+            .json({ status: "200", message: "Records found", routes });
+    } catch (error) {
+        console.error("Error fetching route list:", error);
+        return res.status(500).json({ status: "500", message: "Internal server error" });
+    }
 };
