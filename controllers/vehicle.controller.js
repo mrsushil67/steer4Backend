6d52c06a7fd69abfdb03603b77e55aff72145360
mrsushil67 <mrsushil67@gmail.com>
2025-06-02 14:44:17 +0530

    update vehicle size function

--- controllers/vehicle.controller.js
+++ controllers/vehicle.controller.js
@@ -19,11 +19,15 @@ module.exports.getVehicleList = async (req, res) => {
       limit: 20,
     });
 
-    if(vehicles.length === 0){
-      return res.status(404).json({status: "404", message:"No Record found"})
+    if (vehicles.length === 0) {
+      return res
+        .status(404)
+        .json({ status: "404", message: "No Record found" });
     }
 
-    return res.status(200).json({ status:"200", message:"Record found", vehicles });
+    return res
+      .status(200)
+      .json({ status: "200", message: "Record found", vehicles });
   } catch (error) {
     console.log(error);
     return res
@@ -32,16 +36,29 @@ module.exports.getVehicleList = async (req, res) => {
   }
 };
 
-
 module.exports.getVehicleSize = async (req, res) => {
   try {
-    
-    const vehicleSize = await DBMODELS.Vehiclesize.findAll({});
-    return res.status(200).json({status: "200",vehicleSize})
+    const { size = null || undefined } = req.body || {};
+
+    const whereCondition = size
+      ? {
+          size_title: {
+            [Op.like]: `%${size}%`,
+          },
+        }
+      : {};
+
+    const vehicleSize = await DBMODELS.Vehiclesize.findAll({
+      where: whereCondition,
+    });
+
+    return res
+      .status(200)
+      .json({ status: "200", message: "Record found", vehicleSize });
   } catch (error) {
     console.log(error);
     return res
       .status(500)
       .json({ status: "500", message: "Internal server error" });
   }
-}
+};
