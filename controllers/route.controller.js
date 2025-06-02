31b204a2e591d470e4470b36ecd3992d877f85f7
mrsushil67 <mrsushil67@gmail.com>
2025-06-02 13:22:36 +0530

    update route api

--- controllers/route.controller.js
+++ controllers/route.controller.js
@@ -1,11 +1,28 @@
-const { DBMODELS } = require('../models/init-models');
+const { Op } = require("sequelize");
+const { DBMODELS } = require("../models/init-models");
 
-module.exports.getRoutelist = async(req, res) => {
-    try {
-         const routes = await DBMODELS.RouteMaster.findAll({});
-         return res.status(200).json({ status: "200", message: "Record found", routes})
-    } catch (error) {
-        console.log(error);
-        return res.status(500).json("Internal server error")
-    }
-}
+module.exports.getRoutelist = async (req, res) => {
+  try {
+    const { CustId = null, RouteCat = null } = req.body || {};
+
+    const whereCondition =
+      CustId || RouteCat
+        ? {
+            [Op.or]: [
+              { CustId: CustId },
+              { RouteCat: RouteCat }
+            ]
+          }
+        : {};
+
+    const routes = await DBMODELS.RouteMaster.findAll({
+      where: whereCondition,
+    });
+    return res
+      .status(200)
+      .json({ status: "200", message: "Record found", routes });
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json("Internal server error");
+  }
+};
