14eb7c534483ad29f087b4ce8b5b6d2092c040d9
mrsushil67 <mrsushil67@gmail.com>
2025-06-04 13:06:20 +0530

    update function

--- controllers/tripPlan.controller.js
+++ controllers/tripPlan.controller.js
@@ -1,11 +1,104 @@
 const { DBMODELS } = require("../models/init-models");
 
-module.exports.tripPlan = async (req, res) => {
+module.exports.checkTripPlan = async (req, res) => {
   try {
 
     const trip = await DBMODELS.TripPlanSchedule.findAll({});
     return res.status(200).json({status:"200", trip})
   } catch (error) {
+    console.error("Error While creating tripSheet:", error);
+    return res
+      .status(500)
+      .json({ status: "500", message: "Internal server error" });
+  }
+};
+
+module.exports.tripPlan = async (req, res) => {
+  try {
+
+    const userId = req.user.userId;
+    console.log(userId)
+
+    const {
+      CustType,
+      CustId,
+      RouteId,
+      TripType,
+      VehicleSize,
+      VehicleId,
+      Driver1Id,
+      Driver2Id,
+      VPlaceTime,
+      DepartureTime,
+      Remark,
+      TripSheet,
+      CreatedBy,
+    } = req.body;
+
+    // Validate required fields
+    if (
+      !CustType ||
+      !CustId ||
+      !RouteId ||
+      !TripType ||
+      !VehicleSize ||
+      !VehicleId ||
+      !Driver1Id ||
+      !VPlaceTime ||
+      !DepartureTime ||
+      !TripSheet ||
+      !CreatedBy
+    ) {
+      return res.status(400).json({
+        status: "400",
+        message: "Missing required fields",
+      });
+    }
+
+    console.log(req.body);
+
+    const dataModel = {
+      CustType,
+      CustId,
+      RouteId,
+      TripType,
+      VehicleSize,
+      VehicleId,
+      Driver1Id,
+      Driver2Id,
+      VPlaceTime,
+      DepartureTime,
+      Remark,
+      TripSheet,
+      CreatedBy : userId
+    };
+
+    console.log(dataModel)
+
+    // const data = await DBMODELS.TripPlanSchedule.create(dataModel);
+
+    // return res
+    //   .status(201)
+    //   .json({ status: "201", message: "Record saved successfully", data });
+  } catch (error) {
+    if (error.name === "SequelizeValidationError") {
+      console.error("Validation Error:", error);
+      return res.status(400).json({
+        status: "400",
+        message: "Validation error",
+        details: error.errors.map((err) => err.message),
+      });
+    }
+
+    if (error.name === "SequelizeUniqueConstraintError") {
+      console.error("Unique Constraint Error:", error);
+      return res.status(409).json({
+        status: "409",
+        message: "Duplicate entry error",
+        details: error.errors.map((err) => err.message),
+      });
+    }
+
     console.error("Error While creating tripSheet:", error);
     return res
       .status(500)
