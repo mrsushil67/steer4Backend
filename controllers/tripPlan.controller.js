7c43c91f2403cf2eb5843647a101a2ee8a275099
mrsushil67 <mrsushil67@gmail.com>
2025-06-05 15:39:01 +0530

    create apies for trip update and cancel trip

--- controllers/tripPlan.controller.js
+++ controllers/tripPlan.controller.js
@@ -2,11 +2,23 @@ const { DBMODELS } = require("../models/init-models");
 
 module.exports.checkTripPlan = async (req, res) => {
   try {
+    const { id } = req.query;
 
-    const trip = await DBMODELS.TripPlanSchedule.findAll({});
-    return res.status(200).json({status:"200", trip})
+    console.log(id);
+
+    const trip = id
+      ? await DBMODELS.TripPlanSchedule.findOne({ where: { Id: id } })
+      : await DBMODELS.TripPlanSchedule.findAll({});
+
+    if (!trip || (Array.isArray(trip) && trip.length === 0)) {
+      return res
+        .status(404)
+        .json({ status: "404", message: "No record found" });
+    }
+
+    return res.status(200).json({ status: "200", trip });
   } catch (error) {
-    console.error("Error While creating tripSheet:", error);
+    console.error("Error While fetching trip plan:", error);
     return res
       .status(500)
       .json({ status: "500", message: "Internal server error" });
@@ -15,9 +27,8 @@ module.exports.checkTripPlan = async (req, res) => {
 
 module.exports.tripPlan = async (req, res) => {
   try {
-
     const userId = req.user.userId;
-    console.log(userId)
+    console.log(userId);
 
     const {
       CustType,
@@ -46,7 +57,7 @@ module.exports.tripPlan = async (req, res) => {
       !Driver1Id ||
       !VPlaceTime ||
       !DepartureTime ||
-      !TripSheet||
+      !TripSheet ||
       !StartKm
     ) {
       return res.status(400).json({
@@ -69,7 +80,7 @@ module.exports.tripPlan = async (req, res) => {
       Remark,
       TripSheet,
       StartKm,
-      CreatedBy : userId
+      CreatedBy: userId,
     };
 
     const data = await DBMODELS.TripPlanSchedule.create(dataModel);
@@ -78,16 +89,124 @@ module.exports.tripPlan = async (req, res) => {
       .status(201)
       .json({ status: "201", message: "Record saved successfully", data });
   } catch (error) {
-
-    if (error.name === 'SequelizeUniqueConstraintError') {
+    if (error.name === "SequelizeUniqueConstraintError") {
       return res.status(409).json({
         status: "409",
         message: "Duplicate key error",
       });
     }
-   
+
     console.error("Error While creating tripSheet:", error);
 
+    return res
+      .status(500)
+      .json({ status: "500", message: "Internal server error" });
+  }
+};
+
+module.exports.updateTrip = async (req, res) => {
+  try {
+    const { tripId } = req.query;
+    const updateFields = req.body;
+
+    if(!tripId){
+      return res.status(400).json({
+        status: "400",
+        message: "Missing tripId",
+      });
+    }
+
+    if (Object.keys(updateFields).length === 0) {
+      return res.status(400).json({
+        status: "400",
+        message: "Missing required fields",
+      });
+    }
+
+    const [updatedRows] = await DBMODELS.TripPlanSchedule.update(updateFields, {
+      where: { ID: tripId },
+    });
+
+    if (updatedRows === 0) {
+      return res.status(404).json({
+        status: "404",
+        message: "Record not found",
+      });
+    }
+
+    return res
+      .status(200)
+      .json({
+        status: "200",
+        message: "Record updated successfully",
+        updatedRows,
+      });
+  } catch (error) {
+    console.error("Error While updating trip:", error);
+    return res
+      .status(500)
+      .json({ status: "500", message: "Internal server error" });
+  }
+};
+
+module.exports.cancelTrip = async (req, res) => {
+  try {
+    const { tripId, status } = req.body || {};
+
+    if (!tripId || !status) {
+      return res
+        .status(400)
+        .json({ status: "400", message: "tripId or status missing" });
+    }
+
+    // Check if the status is already the same
+    const existingTripPlanSchedule = await DBMODELS.TripPlanSchedule.findOne({
+      where: { ID: tripId },
+    });
+
+    const existingTripPlan = await DBMODELS.TripPlan.findOne({
+      where: { ID: tripId },
+    });
+
+    console.log("status : ",status,typeof(status))
+    console.log("existingTripPlanSchedule.Status : ",existingTripPlanSchedule.Status);
+    console.log("existingTripPlan.Status : ",existingTripPlan.Status)
+
+    if (
+      (existingTripPlanSchedule && existingTripPlanSchedule.Status === parseInt(status)) ||
+      (existingTripPlan && existingTripPlan.Status === parseInt(status))
+    ) {
+      return res.status(200).json({
+        status: "200",
+        message: `Status is already ${status}`,
+      });
+    }
+
+    const [updatedTripPlanScheduleRows] = await DBMODELS.TripPlanSchedule.update(
+      { Status: status },
+      { where: { ID: tripId } }
+    );
+
+    const [updatedTripPlanRows] = await DBMODELS.TripPlan.update(
+      { Status: status },
+      { where: { ID: tripId } }
+    );
+
+    if (updatedTripPlanScheduleRows === 0 && updatedTripPlanRows === 0) {
+      return res.status(404).json({
+        status: "404",
+        message: "Record not found in both tables",
+      });
+    }
+
+    return res.status(201).json({
+      status: "201",
+      message: "Records updated successfully",
+      updatedTripPlanScheduleRows,
+      updatedTripPlanRows,
+    });
+  } catch (error) {
+    console.error("Error While updating trip:", error);
     return res
       .status(500)
       .json({ status: "500", message: "Internal server error" });
--- routes/tripPlan.route.js
+++ routes/tripPlan.route.js
@@ -4,5 +4,7 @@ const { authorizedUser } = require("../middlewares/authMiddleware");
 
 TripRouter.get("/checkData", TripController.checkTripPlan);
 TripRouter.post("/tripPlan", authorizedUser, TripController.tripPlan); // just check data
+TripRouter.put('/update', TripController.updateTrip);
+TripRouter.post('/cancel', TripController.cancelTrip);
 
 module.exports = TripRouter;
