acc8c0f55a86bfe69700f6e5e84783631409e8f9
mrsushil67 <mrsushil67@gmail.com>
2025-06-06 19:04:15 +0530

    update driver list api and get driver by vehicle assigned by Vid

--- controllers/driver.controller.js
+++ controllers/driver.controller.js
@@ -25,47 +25,82 @@ module.exports.getDriverLisence = async (req, res) => {
 
 module.exports.getDriversList = async (req, res) => {
   try {
-    const { name = null || undefined, licence = null || undefined } =
-      req.body || {};
+    const { VehicleID = null } = req.body || {}
 
-    const whereCondition =
-      name || licence
-        ? {
-            [Op.or]: {
-              DName: {
-                [Op.like]: `%${name}%`,
-              },
-              Licence: {
-                [Op.like]: `%${licence}%`,
-              },
-            },
-          }
-        : {};
+    if (!VehicleID) {
+      return res.status(400).json({ message: "VehicleID is required", status: false });
+    }
+
+    const assignedDrivers = await DBMODELS.DriverAssigntToVehicle.findAll({
+      where: { Vid: VehicleID },
+      attributes: ["Did"],
+    });
+
+    if (assignedDrivers.length === 0) {
+      return res.status(404).json({ status: "404", message: "Not Assign Driver to this Vehicle"});
+    }
+
+    const driverIds = assignedDrivers.map((assignment) => assignment.Did);
 
     const drivers = await DBMODELS.Driver.findAll({
-      where: whereCondition,
+      where: { DriverID: { [Op.in]: driverIds } },
       attributes: ["DriverID", "DName", "Licence"],
-      limit: 20,
     });
 
     if (drivers.length === 0) {
-      return res
-        .status(404)
-        .json({ status: "404", message: "Driver not found" });
+      return res.status(404).json({ message: "No drivers found for the given Driver IDs", status: false });
     }
 
-    const lengths = drivers.length;
-
-    return res.status(200).json({
-      status: "200",
-      lengths,
-      drivers,
-      message: "Drivers fetched successfully",
-    });
+    return res.status(200).json({status: "200", message: "Drivers fetched successfully", drivers});
   } catch (error) {
-    console.log(error);
-    return res
-      .status(500)
-      .json({ status: "500", message: "Internal server error" });
+    console.error("Error fetching drivers list: ", error.message);
+    return res.status(500).json({ message: "Internal Server Error", status: false });
   }
 };
+
+// module.exports.getDriversList = async (req, res) => {
+//   try {
+//     const { VehicleID , name = null || undefined, licence = null || undefined } =
+//       req.body || {};
+
+//     const whereCondition =
+//       name || licence
+//         ? {
+//             [Op.or]: {
+//               DName: {
+//                 [Op.like]: `%${name}%`,
+//               },
+//               Licence: {
+//                 [Op.like]: `%${licence}%`,
+//               },
+//             },
+//           }
+//         : {};
+
+//     const drivers = await DBMODELS.Driver.findAll({
+//       where: whereCondition,
+//       attributes: ["DriverID", "DName", "Licence"],
+//       limit: 20,
+//     });
+
+//     if (drivers.length === 0) {
+//       return res
+//         .status(404)
+//         .json({ status: "404", message: "Driver not found" });
+//     }
+
+//     const lengths = drivers.length;
+
+//     return res.status(200).json({
+//       status: "200",
+//       lengths,
+//       drivers,
+//       message: "Drivers fetched successfully",
+//     });
+//   } catch (error) {
+//     console.log(error);
+//     return res
+//       .status(500)
+//       .json({ status: "500", message: "Internal server error" });
+//   }
+// };
