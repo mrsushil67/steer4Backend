702c082432e1d5f55f6884df198522faeb98c77c
mrsushil67 <mrsushil67@gmail.com>
2025-06-09 10:55:22 +0530

    update method

--- routes/vehicle.route.js
+++ routes/vehicle.route.js
@@ -2,7 +2,7 @@ const VehicleRouter = require("express").Router();
 const VehicleController = require('../controllers/vehicle.controller');
 const { authorizedUser } = require("../middlewares/authMiddleware");
 
-VehicleRouter.get('/vehiclelist', authorizedUser, VehicleController.getVehicleList)
+VehicleRouter.post('/vehiclelist', authorizedUser, VehicleController.getVehicleList)
 VehicleRouter.get('/size', VehicleController.getVehicleSize);
 
 module.exports = VehicleRouter;
