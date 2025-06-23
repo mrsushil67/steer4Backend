d4ec915f6524a75bac2831e4600bdc19a20b624a
mrsushil67 <mrsushil67@gmail.com>
2025-06-23 15:11:33 +0530

    yyyyy

--- controllers/tripExpence.controller.js
+++ controllers/tripExpence.controller.js
@@ -1,74 +1,21 @@
-const { literal, col, where } = require("sequelize");
+const { literal, col, where, fn } = require("sequelize");
 const { DBMODELS } = require("../models/init-models");
 
 module.exports.getTripExpenceList = async (req, res) => {
   try {
     const getAllTripExpence = await DBMODELS.TripOperation.findAll({
+      attributes: [
+        [fn("SUM", col("TripAdvance.Cash")), "totalAdvanceAmount"],
+      ],
       include: [
         {
-          model: DBMODELS.TripPlan,
-          as: "TripPlan",
-          include: [
-            {
-              model: DBMODELS.Vehicle,
-              as: "Vehicle",
-              attributes: ["VehicleID", "VNumer"],
-            },
-            {
-              model: DBMODELS.Driver,
-              as: "Driver",
-              attributes: ["DriverID", "DName"],
-            },
-            {
-              model: DBMODELS.RouteMaster,
-              as: "route_master",
-              attributes: ["RouteId"],
-              include: [
-                {
-                  model: DBMODELS.city,
-                  as: "source_city",
-                  attributes: ["CityId", "CityName", "latitude", "longitude"],
-                },
-                {
-                  model: DBMODELS.city,
-                  as: "dest_city",
-                  attributes: ["CityId", "CityName", "latitude", "longitude"],
-                },
-              ],
-            },
-            {
-              model: DBMODELS.CustRateMap,
-              as: "CustRateMaps",
-              where: {
-                RouteId: col("TripPlan.RouteId"),
-                CustId: col("TripPlan.CustId"),
-                TripType: col("TripPlan.TripType"),
-              },
-              include: [
-                {
-                  model: DBMODELS.TripType,
-                  as: "trip_type",
-                  attributes: ["Id", "TypeName"],
-                },
-              ],
-            //   attributes: [
-            //     "ID",
-            //     "CustId",
-            //     "RouteId",
-            //     "RouteType",
-            //     "TripType",
-            //     "RouteString",
-            //   ],
-            },
-          ],
+          model: DBMODELS.TripAdvance,
+          as: "TripAdvance",
         },
-        {
-            model: DBMODELS.TripAdvance,
-            as: "TripAdvance",
-        }
       ],
+      group: ['TripOperation.id'],
     });
-    const total = getAllTripExpence.length
+    const total = getAllTripExpence.length;
     return res.status(200).json({
       status: "200",
       message: "Trip Expence List",
