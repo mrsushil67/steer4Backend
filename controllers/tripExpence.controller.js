8376a950384f0a4d6c958b7b4f365eddf4b7fa84
mrsushil67 <mrsushil67@gmail.com>
2025-06-23 14:16:16 +0530

    add models

--- controllers/tripExpence.controller.js
+++ controllers/tripExpence.controller.js
@@ -4,70 +4,69 @@ const { DBMODELS } = require("../models/init-models");
 module.exports.getTripExpenceList = async (req, res) => {
   try {
     const getAllTripExpence = await DBMODELS.TripOperation.findAll({
-    //   include: [
-    //     // {
-    //     //   model: DBMODELS.TripPlan,
-    //     //   as: "TripPlan",
-    //     //   include: [
-    //     //     {
-    //     //       model: DBMODELS.Vehicle,
-    //     //       as: "Vehicle",
-    //     //       attributes: ["VehicleID", "VNumer"],
-    //     //     },
-    //     //     // {
-    //     //     //   model: DBMODELS.Driver,
-    //     //     //   as: "Driver",
-    //     //     //   attributes: ["DriverID", "DName"],
-    //     //     // },
-    //     //     // {
-    //     //     //   model: DBMODELS.RouteMaster,
-    //     //     //   as: "route_master",
-    //     //     //   attributes: ["RouteId"],
-    //     //     //   include: [
-    //     //     //     {
-    //     //     //       model: DBMODELS.city,
-    //     //     //       as: "source_city",
-    //     //     //       attributes: ["CityId", "CityName", "latitude", "longitude"],
-    //     //     //     },
-    //     //     //     {
-    //     //     //       model: DBMODELS.city,
-    //     //     //       as: "dest_city",
-    //     //     //       attributes: ["CityId", "CityName", "latitude", "longitude"],
-    //     //     //     },
-    //     //     //   ],
-    //     //     // },
-    //     //     // {
-    //     //     //   model: DBMODELS.CustRateMap,
-    //     //     //   as: "CustRateMaps",
-    //     //     // //   where: {
-    //     //     // //     RouteId: col("TripPlan.RouteId"),
-    //     //     // //     CustId: col("TripPlan.CustId"),
-    //     //     // //     // TripType: col("TripPlan.TripType"),
-    //     //     // //   },
-    //     //     //   include: [
-    //     //     //     {
-    //     //     //       model: DBMODELS.TripType,
-    //     //     //       as: "trip_type",
-    //     //     //       attributes: ["Id", "TypeName"],
-    //     //     //     },
-    //     //     //   ],
-    //     //     // //   attributes: [
-    //     //     // //     "ID",
-    //     //     // //     "CustId",
-    //     //     // //     "RouteId",
-    //     //     // //     "RouteType",
-    //     //     // //     "TripType",
-    //     //     // //     "RouteString",
-    //     //     // //   ],
-    //     //     // },
-    //     //   ],
-    //     // },
-    //     {
-    //         model: DBMODELS.TripAdvance,
-    //         as: "TripAdvance",
-
-    //     }
-    //   ],
+      include: [
+        {
+          model: DBMODELS.TripPlan,
+          as: "TripPlan",
+          include: [
+            {
+              model: DBMODELS.Vehicle,
+              as: "Vehicle",
+              attributes: ["VehicleID", "VNumer"],
+            },
+            // {
+            //   model: DBMODELS.Driver,
+            //   as: "Driver",
+            //   attributes: ["DriverID", "DName"],
+            // },
+            // {
+            //   model: DBMODELS.RouteMaster,
+            //   as: "route_master",
+            //   attributes: ["RouteId"],
+            //   include: [
+            //     {
+            //       model: DBMODELS.city,
+            //       as: "source_city",
+            //       attributes: ["CityId", "CityName", "latitude", "longitude"],
+            //     },
+            //     {
+            //       model: DBMODELS.city,
+            //       as: "dest_city",
+            //       attributes: ["CityId", "CityName", "latitude", "longitude"],
+            //     },
+            //   ],
+            // },
+            // {
+            //   model: DBMODELS.CustRateMap,
+            //   as: "CustRateMaps",
+            // //   where: {
+            // //     RouteId: col("TripPlan.RouteId"),
+            // //     CustId: col("TripPlan.CustId"),
+            // //     // TripType: col("TripPlan.TripType"),
+            // //   },
+            //   include: [
+            //     {
+            //       model: DBMODELS.TripType,
+            //       as: "trip_type",
+            //       attributes: ["Id", "TypeName"],
+            //     },
+            //   ],
+            // //   attributes: [
+            // //     "ID",
+            // //     "CustId",
+            // //     "RouteId",
+            // //     "RouteType",
+            // //     "TripType",
+            // //     "RouteString",
+            // //   ],
+            // },
+          ],
+        },
+        {
+            model: DBMODELS.TripAdvance,
+            as: "TripAdvance",
+        }
+      ],
     });
     const total = getAllTripExpence.length
     return res.status(200).json({
