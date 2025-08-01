6e378a991a5a57cdba005c6cb06b5820ad59c76c
mrsushil67 <mrsushil67@gmail.com>
2025-08-01 15:59:01 +0530

    pass user Authentication

--- routes/tripsettlement.route.js
+++ routes/tripsettlement.route.js
@@ -2,15 +2,15 @@ const SettlemantRouter = require('express').Router();
 const TripSettlementController = require("../controllers/tripsettlement.controller");
 const { authorizedUser } = require("../middlewares/authMiddleware");
 
-SettlemantRouter.post('/getDetailsforTripSettlement', TripSettlementController.getDetailsforTripSettlement);
+SettlemantRouter.post('/getDetailsforTripSettlement',authorizedUser, TripSettlementController.getDetailsforTripSettlement);
 SettlemantRouter.post("/createSettlement", authorizedUser, TripSettlementController.createTripSettlement);
 SettlemantRouter.post('/getSettlement',authorizedUser, TripSettlementController.getTripSettlement);
-SettlemantRouter.post('/getPandingSettlement', TripSettlementController.getPandingSettlementrips);
-SettlemantRouter.post('/getDriverDebit', TripSettlementController.getDriverDebit);
-SettlemantRouter.post('/createDriverDebit', TripSettlementController.createDriverDebit);
-SettlemantRouter.post('/getSettledTrips', TripSettlementController.getSettledTrips);
+SettlemantRouter.post('/getPandingSettlement',authorizedUser, TripSettlementController.getPandingSettlementrips);
+SettlemantRouter.post('/getDriverDebit',authorizedUser, TripSettlementController.getDriverDebit);
+SettlemantRouter.post('/createDriverDebit',authorizedUser, TripSettlementController.createDriverDebit);
+SettlemantRouter.post('/getSettledTrips',authorizedUser, TripSettlementController.getSettledTrips);
 SettlemantRouter.post('/updateSettlement',authorizedUser, TripSettlementController.UpdateTripSettlement);
-SettlemantRouter.post('/updateDriverDebit', TripSettlementController.updateDriverDebit);
+SettlemantRouter.post('/updateDriverDebit',authorizedUser, TripSettlementController.updateDriverDebit);
 
 module.exports = SettlemantRouter;
 
